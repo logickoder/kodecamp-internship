a68fa6c09ecd293717b26d10b124ef16a1197649
logickoder <chukwudumebiorazulike@gmail.com>
2022-04-09 19:09:53 +0100

    [chore] minor changes

--- mycanva/src/main/java/dev/logickoder/mycanva/MyCanvaView.kt
+++ mycanva/src/main/java/dev/logickoder/mycanva/MyCanvaView.kt
@@ -1,34 +1,34 @@
 package dev.logickoder.mycanva
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 import kotlin.math.abs
 
+private const val STROKE_WIDTH = 12f
+
 class MyCanvaView(context: Context) : View(context) {
-    private lateinit var extraCanvas: Canvas
-    private lateinit var extraBitmap: Bitmap
     private lateinit var frame: Rect
 
-    private val STROKE_WIDTH = 12f
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
-
     private var currentX = 0f
     private var currentY = 0f
 
-    private val bgColor = ResourcesCompat.getColor(resources, R.color.purple_200, null)
+    private val bgColor = ResourcesCompat.getColor(resources, R.color.white, null)
     private val drawColor = ResourcesCompat.getColor(resources, R.color.black, null)
 
     private var path = Path()
 
     private val drawing = Path()
-    private val currentPath = Path()
 
     private val paint = Paint().apply {
         color = drawColor
@@ -42,23 +42,20 @@ class MyCanvaView(context: Context) : View(context) {
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
-        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        extraCanvas = Canvas(extraBitmap)
-
-        extraCanvas.drawColor(bgColor)
-
         //create a rectangular frame around the drawing
-        val inset = 40
-        frame = Rect(inset, inset, w - inset, h - inset)
+        val inset = 50
+        frame = Rect(inset, inset * 2, w - inset, h - inset * 2)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
-//        canvas?.drawPath(drawing, paint)
-//        canvas?.drawPath(currentPath, paint)
-        canvas?.drawRect(frame, paint)
+//        canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas?.run {
+            drawColor(bgColor)
+            drawPath(drawing, paint)
+            drawPath(path, paint)
+            drawRect(frame, paint)
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -67,19 +64,19 @@ class MyCanvaView(context: Context) : View(context) {
             motionTouchEventY = event.y
         }
 
-
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> touchStart()
-            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_MOVE -> if (inFrame()) touchMove()
             MotionEvent.ACTION_UP -> touchUp()
         }
+        invalidate()
 
         return true
     }
 
     private fun touchUp() {
-        drawing.addPath(currentPath)
-        currentPath.reset()
+        drawing.addPath(path)
+        path.reset()
     }
 
     private fun touchMove() {
@@ -95,11 +92,7 @@ class MyCanvaView(context: Context) : View(context) {
             )
             currentX = motionTouchEventX
             currentY = motionTouchEventY
-
-            extraCanvas.drawPath(path, paint)
         }
-
-        invalidate()
     }
 
     private fun touchStart() {
@@ -107,5 +100,13 @@ class MyCanvaView(context: Context) : View(context) {
         path.moveTo(motionTouchEventX, motionTouchEventY)
         currentX = motionTouchEventX
         currentY = motionTouchEventY
+    }
+
+    /**
+     * True if the current stroke is in drawing frame
+     */
+    private fun inFrame(): Boolean {
+        return motionTouchEventX.let { it > frame.left && it < frame.right } &&
+                motionTouchEventY.let { it > frame.top && it < frame.bottom }
     }
 }
